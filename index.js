e3cbe20a9f47959c5f81578c5cb88d033b672c29
tubisaffo <tubisaffo@gmail.com>
2024-03-27 12:12:27 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(function(year) { return year.result === "W" });
+    if (!win) {
+        return undefined
+    }
+    return win.year
+
+}
